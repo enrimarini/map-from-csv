9216699186bf5467c4e2d8edc3370df37888e48a
enrimarini <98195595+enrimarini@users.noreply.github.com>
2023-04-25 11:29:53 -0400

    Update generatemap.py

--- generatemap.py
+++ generatemap.py
@@ -80,4 +80,4 @@ def update_map(selected_months, selected_years, selected_time_of_day):
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, host='0.0.0.0', port=10000)
